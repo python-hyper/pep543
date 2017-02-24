116a907cbd685bdf68cbae73d7c68d7f64455381
Cory Benfield <lukasaoz@gmail.com>
2017-02-24 15:01:01 +0000

    Initial classes for stdlib PEP543 shim

---
+++ pep543/stdlib.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Stdlib Shim
+~~~~~~~~~~~
+
+This module shims the standard library OpenSSL module into the PEP 543 API.
+"""
+import ssl
+
+from . import (
+    Backend, ClientContext, ServerContext, Certificate, PrivateKey, TrustStore
+)
+
+class OpenSSLClientContext(ClientContext):
+    """
+    This class controls and creates wrapped sockets and buffers for using the
+    standard library bindings to OpenSSL to perform TLS connections on the
+    client side of a network connection.
+    """
+    def __init__(self, configuration):
+        self._configuration = configuration
+
+    @property
+    def configuration(self):
+        return self._configuration
+
+
+class OpenSSLServerContext(ServerContext):
+    """
+    This class controls and creates wrapped sockets and buffers for using the
+    standard library bindings to OpenSSL to perform TLS connections on the
+    server side of a network connection.
+    """
+    def __init__(self, configuration):
+        self._configuration = configuration
+
+    @property
+    def configuration(self):
+        return self._configuration
+
+
+class OpenSSLCertificate(Certificate):
+    """
+    A handle to a certificate object, either on disk or in a buffer, that can
+    be used for either server or client connectivity.
+    """
+    pass
+
+
+class OpenSSLPrivateKey(PrivateKey):
+    """
+    A handle to a private key object, either on disk or in a buffer, that can
+    be used along with a certificate for either server or client connectivity.
+    """
+    pass
+
+
+class OpenSSLTrustStore(TrustStore):
+    """
+    A handle to a trust store object, either on disk or the system trust store,
+    that can be used to validate the certificates presented by a remote peer.
+    """
+    pass
+
+
+#: The stdlib ``Backend`` object.
+STDLIB_BACKEND = Backend(
+    client_context=OpenSSLClientContext,
+    server_context=OpenSSLServerContext,
+    certificate=OpenSSLCertificate,
+    private_key=OpenSSLPrivateKey,
+    trust_store=OpenSSLTrustStore
+)
