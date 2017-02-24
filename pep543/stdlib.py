6055ab7d052887c4a9b0907af48a9cbf78708196
Cory Benfield <lukasaoz@gmail.com>
2017-02-24 15:33:16 +0000

    Shell of TLSWrappedBuffer

--- pep543/stdlib.py
+++ pep543/stdlib.py
@@ -8,8 +8,61 @@ This module shims the standard library OpenSSL module into the PEP 543 API.
 import ssl
 
 from . import (
-    Backend, ClientContext, ServerContext, Certificate, PrivateKey, TrustStore
+    Backend,
+    Certificate,
+    ClientContext,
+    PrivateKey,
+    ServerContext,
+    TLSWrappedBuffer,
+    TrustStore
 )
+
+
+class OpenSSLWrappedBuffer(TLSWrappedBuffer):
+    """
+    An in-memory TLS implementation based on OpenSSL.
+    """
+    def __init__(self, parent_context, ssl_context):
+        self._parent_context = parent_context
+        self._ssl_context = ssl_context
+
+    def read(self, amt):
+        pass
+
+    def readinto(self, buffer, amt):
+        pass
+
+    def write(self, buf):
+        pass
+
+    def do_handshake(self):
+        pass
+
+    def cipher(self):
+        pass
+
+    def negotiated_protocol(self):
+        pass
+
+    @property
+    def context(self):
+        return self._parent_context
+
+    def negotiated_tls_version(self):
+        pass
+
+    def shutdown(self):
+        pass
+
+    def receive_from_network(self, data):
+        pass
+
+    def peek_outgoing(self, data):
+        pass
+
+    def consume_outgoing(self, data):
+        pass
+
 
 class OpenSSLClientContext(ClientContext):
     """
