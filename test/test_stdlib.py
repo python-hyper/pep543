d0e15bf512b8277686fe51f276df77feb57c91a4
Cory Benfield <lukasaoz@gmail.com>
2017-03-08 13:38:20 +0000

    Fixup docstrings

--- test/test_stdlib.py
+++ test/test_stdlib.py
@@ -19,7 +19,7 @@ CONTEXTS = (
 def wrap_buffers(context):
     """
     A convenient helper that calls wrap_buffers with the appropriate number of
-    arugments.
+    arguments.
     """
     if isinstance(context, pep543.stdlib.STDLIB_BACKEND.client_context):
         context.wrap_buffers(server_hostname=None)
@@ -44,8 +44,8 @@ class TestStdlibErrorHandling(object):
     @pytest.mark.parametrize('context', CONTEXTS)
     def test_bad_values_for_versions_client(self, lowest, highest, context):
         """
-        Using TLSConfiguration objects with a bad value for their minimum
-        version raises a TLSError with Client contexts.
+        Using TLSConfiguration objects with a bad value for their minimum or
+        maximum version raises a TLSError with Client contexts.
         """
         config = pep543.TLSConfiguration(
             validate_certificates=False,
@@ -59,7 +59,7 @@ class TestStdlibErrorHandling(object):
     @pytest.mark.parametrize('context', CONTEXTS)
     def test_no_supported_cipher_suites(self, context):
         """
-        Using TLSConfiguration objects that have only unsupporetd cipher suites
+        Using TLSConfiguration objects that have only unsupported cipher suites
         raises a TLSError.
         """
         # We assume that no cipher suite will be defined with the code eeee.
